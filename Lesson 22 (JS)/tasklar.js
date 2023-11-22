5a5d0ff53eb86bc367915d996c6e0a80112d5592
NigarValikhanova <imnigarvalikhanova@gmail.com>
2023-11-23 01:49:47 +0400

    JS Learning

---
+++ Lesson 22 (JS)/tasklar.js
@@ -0,0 +1,142 @@
+
+//🆘🆘🆘🆘🆘🆘🆘🆘🆘🆘🆘ŞƏRTLƏR VƏ QAYDALAR🆘🆘🆘🆘🆘🆘🆘🆘🆘🆘🆘🆘🆘 */
+// !HECKES BIRBIRINDEN TASKIN KONKRET HELLINI SORUSHMUR VE KOPYALAMIR
+// !ANLAMADIGINIZI BIRBASHA MENDEN SORUSHMURSUZ WHATSAPP QRUPUNDA MUZAKIRE EDIRSIZ TASKLAR BAREDE HECKESIN MESAJINI SHEXSIDE CAVABLAMAYACAM :D
+// !BUTUN NETICELER console.log() VASITESIYLE GOSTERILMELIDIR
+// !DERSDE KECMEDIYIMIZ MOVZULARA AID KODLARDAN MAXIMUM UZAG DURMAGA CALISHIN
+// !BUTUN TASKLARI 1 1 OXUYUN HAMSINA EYNI VAXTDA BAXMAYIN
+// !HER BIR TASKIN HELLI TASKIN OZUNUN ALTINDA YAZILMALI NOVBETI TASKA KECENDE BITIRILEN TASK COMMENTE ALINMALIDIR.
+
+// full name numunesi
+// let fullName = 'Murad Nərimanlı Tərlan'
+
+//1) AD soyad ata adinizi butov sheklde goturun ve array sheklinde herflerine ayirin
+
+
+//2) Herflere ayrilmish arrayda soyadinizi adinizin onune keciren alqoritm yazin
+
+
+//3) Alinan yeni arrayi stringe cevirin ama ilk array deyishmesin
+
+let arr = [4,5,2,1,5,2,5,3,5,6,2,1,6,2,6,2,5,3,2,7,4,6,4,5,6,2,5,6,3,7,3,7]
+//4) Yuxaridaki arrayda 5 reqeminin nece defe tekrarlandigini tapin
+
+
+//5) Yuxaridaki arraydaki butun reqemlerin cemini tapin
+
+
+//6) arrayda tekrar olunan reqemleri artan sira ile duzun
+
+
+//7) arraydaki en boyuk reqemi tapin ve nece defe tekrarlandigini gosterin
+
+
+//8) Random 1 den 10 a qeder reqem generate edin ve sonra arrayda hemin reqemin olub olmamasini yoxlayin
+
+
+//9) Adinizdaki herflerin sayini tapin ve hemin sayin arrayda olub olmamasini yoxlayin Meselen Murad adinda 5 herf var ve 5 arrayda var
+
+
+//10) arraydaki ilk 3 e bolunende qaliqda 2 alinan reqemi ve hemin reqemin arraydaki indexini tapin
+
+
+//11) arraydaki en boyuk reqemin ilk indexini tapin
+
+
+//12) 4 reqeminin arrayin hansi indexlerinde oldugunu gosterin
+
+//13) 5 reqeminin arraydaki en boyuk ve en kicik indexlerini tapin
+
+
+//14) "arr" - arrayindan reqemleri 2 den boyuk olan yeni array yaradin ve alinmish arrayla "arr" arrayinin uzunluqlari ferqini hesablayin
+
+
+
+//15) 7 reqeminin indexleri cemini tapin.
+
+
+/////////////////////////////////////////////////////
+//16)
+let arr2 = [
+  {
+    name:'test',
+    key:1
+  },
+  {
+    name:'task',
+    key:2
+  },
+  {
+    name:'tanqo',
+    key:3
+  },
+  {
+    name:'like',
+    key:4
+  },
+  {
+    name:'task',
+    key:5
+  },
+  {
+    name:'trust',
+    key:6
+  },
+  {
+    name:'test',
+    key:7
+  },
+  {
+    name:'last',
+    key:8
+  },
+  {
+    name:'tanqo',
+    key:9
+  },
+  {
+    name:'elephant',
+    key:10
+  },
+  {
+    name:'love',
+    key:11
+  },
+  {
+    name:'small',
+    key:12
+  },
+  {
+    name:'little',
+    key:13
+  },
+]
+
+
+//QEYD Bu tasklarda arr2 istifade edilecekdir
+// 16 arr2 de "name"-i "t" herfi ile bashlayan obyektleri yeni arraya yigin
+
+
+// 17 arr2 de "name"-i "t" herfi ile bashlayib "t" herfi ile biten obyektlerin sayini tapin
+
+
+// 18 arr2 de "name"-i "t" herfi ile bashlayib "t" herfi ile biten obyektlerin "key"- lerinin cemini tapin
+
+// 19 arr2 de "name"-i "e" herfi ile biten obyeklerdeki name-in deyerini "SuperDev" sozu ile evezleyin.
+
+// 20 arr2 de "name"-i en uzun olan obyektin key-i ni tapin
+
+// 21 arr2 de "name"-i en uzun olan obyektin indexin kvadratini hesablayin
+
+// 22 arr2 de "name"-inin uzunlugu 4 olan obyektlerden ibaret yeni array yaradin.
+
+// 23 arr2 de en boyuk "key" - i olan obyektin "name"-i ni tapin
+
+// 24 arr2 de terkibinde 2 'L' herfi olan obyekt(ler)in index(ler)ini tapin.
+
+
+// 25 arr2 de terkibinde 2 't' herfi olan obyekt(ler)in key(ler)ini tapin.
+
+// 26 Bura da ozunuz arr2 ile ede bileceyiniz en maragli yeni funksionalligi yazin.
+
+
