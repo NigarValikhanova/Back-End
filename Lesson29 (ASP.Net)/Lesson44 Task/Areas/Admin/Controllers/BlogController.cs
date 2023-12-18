aabb5d479ed60628f8772d1cf3401a25045e1cda
NigarValikhanova <imnigarvalikhanova@gmail.com>
2023-12-18 14:38:44 +0400

    New Task about ASP.Net working admin panel

---
+++ Lesson29 (ASP.Net)/Lesson44 Task/Areas/Admin/Controllers/BlogController.cs
@@ -0,0 +1,35 @@
+﻿using Lesson43_Task.DAL;
+using Lesson43_Task.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson43_Task.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BlogController : Controller
+    {
+        private readonly AppDbContext _appContext;
+        public BlogController(AppDbContext appContext)
+        {
+            _appContext = appContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_appContext.Blogs.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Blog blog)
+        {
+            _appContext.Blogs.Add(blog);
+            _appContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Lesson29 (ASP.Net)/Lesson44 Task/DAL/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Lesson43_Task.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lesson43_Task.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Blog> Blogs { get; set; }
+
+    }
+}
